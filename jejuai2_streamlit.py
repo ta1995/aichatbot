779a678a6fadda01ad1ab4d5ef29e5d6c6568a0b
ta1995 <78491560+ta1995@users.noreply.github.com>
2024-10-31 16:29:06 +0900

    Update jejuai2_streamlit.py

--- jejuai2_streamlit.py
+++ jejuai2_streamlit.py
@@ -18,32 +18,32 @@ def classify(text):
 
 
 # CHANGE THIS to something you want your machine learning model to classify
-while True:
+# while True:
     #question = input("제주도에 대해 궁금한 것을 물어봐 주세요>>")
     question = st.text_input("제주도에 대해 궁금한 것을 물어봐 주세요>> ", "")
 
     #if question != "":
-    if len(question)>0:    
-        #if (question == "나가기"):
-        #  break
+if len(question)>0:    
+    #if (question == "나가기"):
+    #  break
 
-        demo = classify(question)
-        label = demo["class_name"]
-        confidence = demo["confidence"]
+    demo = classify(question)
+    label = demo["class_name"]
+    confidence = demo["confidence"]
 
-        # if confidence < 70:
-        #     st.write("질문을 이해하지 못했어요. 다시 질문해 주시겠어요?")
-        # elif label == "food":
-        #     st.write("제주도하면 고기국수를 먹어보세요. 어디든 다 맛있답니다. 유명 흑되지집에서 먹어보는 것도 좋겠습니다.")
-        #     st.write("답변 정확되:", confidence)
-        # elif label == "cafe":
-        #     st.write("애월에는 예쁘고 유명한 카페들이 많아요. 어디든 전망도 졸고 커피도 맛있으니 그 주에서 골라보세요. 참, 요즘엔 도넛집도 핫하다고 해요.")
-        #     st.write("답변 정확되:", confidence)
-        # elif label == "hotplace":
-        #     st.write("제주도에서 올레길 한번은 걸어보는 것도 좋겠죠? 저녁에 야시장을 둘러 보는 것도 추천해요. 무엇보다 제주하면 바다죠? 예쁜 세화해변도 추천해요.")
-        #     st.write("답변 정확되:", confidence)
+    # if confidence < 70:
+    #     st.write("질문을 이해하지 못했어요. 다시 질문해 주시겠어요?")
+    # elif label == "food":
+    #     st.write("제주도하면 고기국수를 먹어보세요. 어디든 다 맛있답니다. 유명 흑되지집에서 먹어보는 것도 좋겠습니다.")
+    #     st.write("답변 정확되:", confidence)
+    # elif label == "cafe":
+    #     st.write("애월에는 예쁘고 유명한 카페들이 많아요. 어디든 전망도 졸고 커피도 맛있으니 그 주에서 골라보세요. 참, 요즘엔 도넛집도 핫하다고 해요.")
+    #     st.write("답변 정확되:", confidence)
+    # elif label == "hotplace":
+    #     st.write("제주도에서 올레길 한번은 걸어보는 것도 좋겠죠? 저녁에 야시장을 둘러 보는 것도 추천해요. 무엇보다 제주하면 바다죠? 예쁜 세화해변도 추천해요.")
+    #     st.write("답변 정확되:", confidence)
 
-        # CHANGE THIS to do something different with the result
-        st.write("result: '%s' with %d%% confidence" % (label, confidence))
-    else:
-        st.write("검색어 입력")
+    # CHANGE THIS to do something different with the result
+    st.write("result: '%s' with %d%% confidence" % (label, confidence))
+else:
+    st.write("검색어 입력")
